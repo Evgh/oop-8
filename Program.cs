44422e26990720598d1522e3e721b881fa5b66b2
Evgh <kasperovich.official@gmail.com>
2020-11-13 17:38:42 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace oop_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello f World!");
+        }
+    }
+}
